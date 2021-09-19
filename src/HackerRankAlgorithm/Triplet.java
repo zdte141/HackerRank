4fa675425529270268c19c53ccfd5b7f25cce844
sean112533 <zdte141@live.rhul.ac.uk>
2021-09-19 17:43:40 +0100

    make another folder for the HackerRank folder

---
+++ src/HackerRankAlgorithm/Triplet.java
@@ -0,0 +1,5 @@
+package HackerRankAlgorithm;
+
+public class Triplet {
+
+}
---
+++ src/HackerRankTest/TestTriplet.java
@@ -0,0 +1,20 @@
+package HackerRankTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import HackerRankAlgorithm.Triplet;
+
+class TestTriplet {
+  @Before
+  public void testClass() {
+    Triplet triplet = new Triplet();
+  }
+
+  @Test
+  public void testTriplet() {
+    Triplet test = new Triplet();
+
+
+  }
+
+}
