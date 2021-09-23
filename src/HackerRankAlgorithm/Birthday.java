9cb2972c5dc92b870d700f3731e3799b5cbc71fb
sean112533 <zdte141@live.rhul.ac.uk>
2021-09-23 02:32:46 +0100

    commit Birthday in different approach

--- src/HackerRankAlgorithm/Birthday.java
+++ src/HackerRankAlgorithm/Birthday.java
@@ -1,21 +1,29 @@
 package HackerRankAlgorithm;
 
+import java.util.List;
+
 public class Birthday {
 
-  public int birthday(int[] n) {
+  public int birthday(List<Integer> candles) {
     int frequency = 0;
     int max = 0;
 
-    for (int i = 0; i < n.length; i++) {
-      max = Math.max(max, n[i]);
+    for (int i = 0; i < candles.size(); i++) {
+      max = Math.max(max, candles.get(i));
     }
-    for (int j = 0; j < n.length; j++) {
-      if (max == n[j]) {
+    for (int j = 0; j < candles.size(); j++) {
+      if (max == candles.get(j)) {
         frequency++;
       }
     }
-    System.out.println(frequency);
     return frequency;
   }
+  /**
+   * public int birthday(int[] n) { int frequency = 0; int max = 0;
+   * 
+   * for (int i = 0; i < n.length; i++) { max = Math.max(max, n[i]); } for (int j = 0; j < n.length;
+   * j++) { if (max == n[j]) { frequency++; } } System.out.println(frequency); return frequency; }
+   */
+
 
 }
--- src/HackerRankTest/TestBirthday.java
+++ src/HackerRankTest/TestBirthday.java
@@ -1,6 +1,8 @@
 package HackerRankTest;
 
 import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import HackerRankAlgorithm.Birthday;
@@ -14,14 +16,25 @@ public class TestBirthday {
   @Test
   public void testBirthday() {
     Birthday test = new Birthday();
+    /**
+     * int[] n = {3, 2, 1, 3};
+     * 
+     * int output = 2;
+     * 
+     * assertTrue("check if the method return the most common height of candles", test.birthday(n)
+     * == output);
+     */
 
-    int[] n = {3, 2, 1, 3};
+    List<Integer> candles = new ArrayList<Integer>();
+    candles.add(3);
+    candles.add(2);
+    candles.add(1);
+    candles.add(3);
 
     int output = 2;
 
     assertTrue("check if the method return the most common height of candles",
-        test.birthday(n) == output);
-
+        test.birthday(candles) == output);
   }
 
 }
